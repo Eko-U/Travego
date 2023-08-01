3c30a470d1bd0dcfddd31e5e8e777fbf151682d0
Eko Gift <ekogift2000@gmail.com>
2023-08-01 15:03:00 +0100

    update sidebar ui

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 const sidebar = document.querySelector('.nav-menu-btn');
 const navMenu = document.querySelector('.sidebar');
 const sidebarClose = document.querySelector('.sidebar-close');
+const overlay = document.querySelector('.overlay');
 console.log(sidebar);
 
 sidebar.addEventListener('click', function () {
   navMenu.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
 });
 
 sidebarClose.addEventListener('click', function () {
   navMenu.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
 });
