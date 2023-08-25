be467e660ec473207c85daea83464228316829a1
Eko Gift <ekogift2000@gmail.com>
2023-08-25 10:36:47 +0100

    update ui and script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const sidebar = document.querySelector('.nav-menu-btn');
+const header = document.querySelector('.header');
 const navMenu = document.querySelector('.sidebar');
 const sidebarClose = document.querySelector('.sidebar-close');
 const overlay = document.querySelector('.overlay');
@@ -14,3 +15,8 @@ sidebarClose.addEventListener('click', function () {
   navMenu.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
 });
+
+// Get current year
+const yearEl = [...document.querySelectorAll('.year')];
+const curYear = new Date().getFullYear();
+yearEl.forEach((el) => (el.textContent = curYear));
