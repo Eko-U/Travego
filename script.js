68e3d2630cf91dc7aec7a1dfb51a11a5a5048fdb
Eko Gift <ekogift2000@gmail.com>
2023-08-29 23:21:22 +0100

    add cool js features

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const navMenu = document.querySelector('.sidebar');
 const sidebarClose = document.querySelector('.sidebar-close');
 const overlay = document.querySelector('.overlay');
 
+const nav = document.querySelector('.nav');
+const sideMenu = document.querySelector('.sidebar-menu');
+
+const btnHero = document.querySelector('.hero__btn');
+const sectionDestination = document.querySelector('.section-destination');
+
 sidebar.addEventListener('click', function () {
   navMenu.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
@@ -20,3 +26,71 @@ sidebarClose.addEventListener('click', function () {
 const yearEl = [...document.querySelectorAll('.year')];
 const curYear = new Date().getFullYear();
 yearEl.forEach((el) => (el.textContent = curYear));
+
+///////////////////////////////////////////////////
+// scrollTo
+
+btnHero.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const sectionBound = sectionDestination.getBoundingClientRect();
+
+  window.scrollTo({
+    left: sectionBound.left + window.pageXOffset,
+    top: sectionBound.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
+
+// nav.addEventListener('click', function (e) {
+//   e.preventDefault();
+
+//   if (e.target.classList.contains('nav__link')) {
+//     const id = e.target.getAttribute('href');
+//     const scoord = document.querySelector(id).getBoundingClientRect();
+
+//     window.scrollTo({
+//       left: scoord.left + window.pageXOffset,
+//       top: scoord.top + window.pageYOffset,
+//       behavior: 'smooth',
+//     });
+//   }
+// });
+
+nav.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    const scoord = document.querySelector(id).getBoundingClientRect();
+
+    window.scrollTo({
+      left: scoord.left + window.pageXOffset,
+      top: scoord.top + window.pageYOffset,
+      behavior: 'smooth',
+    });
+  }
+});
+
+const sideBarMenu = document.querySelector('.sidebar-menu');
+
+sideBarMenu.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('sidebar__link')) {
+    const id = e.target.getAttribute('href');
+    const scoord = document.querySelector(id).getBoundingClientRect();
+
+    console.log(id);
+
+    window.scrollTo({
+      left: scoord.left + window.pageXOffset,
+      top: scoord.top + window.pageYOffset,
+      behavior: 'smooth',
+    });
+
+    overlay.classList.add('hidden');
+    navMenu.classList.add('hidden');
+  }
+});
+sideMenu;
